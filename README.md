4e2b4ee99110f182e76fe81fc837268ed5cafdad
Dmitry Podlesny <rezed93@gmail.com>
2018-07-22 03:56:33 +0300

    update for gh-pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Hello!
+# Hello!
 
 Here I'm practicing in some math and physics programming while watching [Coding Train Videos](https://www.youtube.com/user/shiffman/videos).
+
+### [Check out this on Github Pages](https://manneredboor.github.io/coding-train-practice/)
--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,3 +1,5 @@
-export const home = '/'
-export const vectors = '/vectors/'
-export const forces = '/forces/'
+const withBase = (s: string) => process.env.BASE_URL + s
+
+export const home = withBase('/')
+export const vectors = withBase('/vectors/')
+export const forces = withBase('/forces/')

369a604f907da2312bf26e08d2f194600c233860
Dmitry Podlesny <rezed93@gmail.com>
2018-07-30 19:05:07 +0300

    gnarly-app-tools update

---
+++ gnarly.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Server listening
+  host: '0.0.0.0',
+  port: 8080,
+
+  // Url prefix
+  baseUrl: '/coding-train-practice',
+
+  // For static renderer, used when you run `npm run build-static` or `build-all`
+  routerConfig: './src/routes',
+
+  // Serve static with express (do not enable if static served with nginx for example)
+  serveStatic: true,
+
+  // Add babel transform runtime
+  babelRuntime: true,
+}

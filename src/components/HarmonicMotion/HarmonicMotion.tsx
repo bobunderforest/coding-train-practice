92092740023731270b8e7d8712b5b4a3fe74c8c3
Dmitry Podlesny <rezed93@gmail.com>
2018-08-09 20:26:39 +0300

    HarmonicMotion update

--- src/components/HarmonicMotion/HarmonicMotion.tsx
+++ src/components/HarmonicMotion/HarmonicMotion.tsx
@@ -4,11 +4,11 @@ import { Vector } from '../Vectors/VectorMutable'
 // import { random } from 'utils'
 import { colors } from 'utils/styles'
 
-const AMPLITUDE = 300
+const AMPLITUDE = 100
 const PERIOD = 3000
 
 interface DrawState {
-  ballPos: Vector
+  balls: Vector[]
 }
 
 const Page = DemoPage as new () => DemoPage<DrawState>
@@ -17,34 +17,40 @@ export const HarmonicMotion = () => (
   <Page
     // next={links.dragResistance}
     srcLink="HarmonicMotion/HarmonicMotion.tsx"
-    canvasProps={({ drawState }) => ({
-      // onMouseDown: () => (drawState.fire = true),
-      // onMouseMove: e => {
-      //   drawState.mouse.x = e.pageX
-      //   drawState.mouse.y = e.pageY
-      // },
-    })}
+    // canvasProps={({ drawState }) => ({
+    //   onMouseDown: () => (drawState.fire = true),
+    //   onMouseMove: e => {
+    //     drawState.mouse.x = e.pageX
+    //     drawState.mouse.y = e.pageY
+    //   },
+    // })}
     setup={({ width, height }) => ({
-      ballPos: new Vector(0, 0),
+      balls: [...Array(21)].map((_, i, a) => new Vector(0, (i - a.length / 2) * 50)),
       // mouse: new Vector(width / 2, height / 2),
     })}
     render={({ ctx, width, height, drawState, time }) => {
-      const { ballPos } = drawState
+      const { balls } = drawState
 
-      const x = AMPLITUDE * Math.sin((time / PERIOD) * (2 * Math.PI))
-
+      ctx.save()
       ctx.translate(width / 2, height / 2)
+      balls.forEach((ball, i) => {
+        const x = AMPLITUDE * Math.sin((time / PERIOD) * (2 * Math.PI) + i * 100)
 
-      ctx.beginPath()
-      ctx.moveTo(0, 0)
-      ctx.lineTo(x, ballPos.y)
-      ctx.strokeStyle = '#ccc'
-      ctx.stroke()
+        ctx.beginPath()
 
-      ctx.beginPath()
-      ctx.arc(x, ballPos.y, 40, 0, 360)
-      ctx.fillStyle = colors.brand
-      ctx.fill()
+        ctx.moveTo(0, ball.y)
+        ctx.lineTo(x, ball.y)
+
+        ctx.strokeStyle = '#ccc'
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(x, ball.y, 20, 0, 360)
+        ctx.fillStyle = colors.brand
+        ctx.fill()
+      })
+      ctx.restore()
+
     }}
   />
 )
